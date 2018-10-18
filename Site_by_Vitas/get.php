86a689fd36498aa5b3bb68897b422e798b9bb4ba
Kozzlv <VITAS_I_Andrey@mail.ru>
2018-10-19 00:37:38 +0300

    Delete get.php

--- Site_by_Vitas/get.php
+++
@@ -1,7 +0,0 @@
-<?php
-$name = "Не известно";
-$age = "Не известно";
-if(isset($_GET['name'])) $name = $_GET['name'];
-if (isset($_GET['age'])) $age = $_GET['age'];
-echo "Ваше имя: $name  <br> Ваш возраст: $age";
-?>
