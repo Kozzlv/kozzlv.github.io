f27ce66fcffb0bcd27f4d085f5ed0fc8fe3779e4
Kozzlv <VITAS_I_Andrey@mail.ru>
2018-10-19 00:06:01 +0300

    Update get.php

--- Site_by_Vitas/get.php
+++ Site_by_Vitas/get.php
@@ -1,14 +1,5 @@
 <?php
-$login = "не определен";
-$age = "не определен";
-if(isset($_GET['login'])){
- 
-    $login = $_GET['login'];
-}
-if(isset($_GET['age'])){
- 
-    $age = $_GET['age'];
-}
-    echo "Ваш логин: $login <br> Ваш возраст: $age";
-    header( "Location: http://mail.ru/" );
+$new_url = 'https://example.com/final.php';
+header('Location: '.$new_url);
+exit();
 ?>
